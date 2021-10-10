217f3a63e2c2b80a1fea032e939d19abcb5a60b0
Domaman202 <vip.domaman@mail.ru>
2021-10-10 20:50:03 +0700

    Update MachineBlock.java

--- src/main/java/ru/DmN/core/api/block/MachineBlock.java
+++ src/main/java/ru/DmN/core/api/block/MachineBlock.java
@@ -1,29 +1,28 @@
 package ru.DmN.core.api.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.enums.WallMountLocation;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 import ru.DmN.core.api.block.entity.MachineBlockEntity;
 
-public abstract class MachineBlock extends Block implements BlockEntityProvider {
+public abstract class MachineBlock extends HorizontalFacingBlock implements BlockEntityProvider, InventoryProvider {
     public BlockItem item;
 
     public MachineBlock(Settings settings, BlockItem item) {
         super(settings.hardness(1).requiresTool());
         this.item = item;
     }
-
-    @Nullable
-    @Override
-    public abstract MachineBlockEntity createBlockEntity(BlockPos pos, BlockState state);
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
@@ -46,7 +45,16 @@ public abstract class MachineBlock extends Block implements BlockEntityProvider 
     }
 
     @Override
+    public SidedInventory getInventory(BlockState state, WorldAccess world, BlockPos pos) {
+        return ((MachineBlockEntity) world.getBlockEntity(pos)).getInventory(state, world, pos);
+    }
+
+    @Override
     public BlockItem asItem() {
         return item;
     }
+
+    @Nullable
+    @Override
+    public abstract MachineBlockEntity createBlockEntity(BlockPos pos, BlockState state);
 }
