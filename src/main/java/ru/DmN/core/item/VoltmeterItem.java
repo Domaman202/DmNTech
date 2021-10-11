11051b0d4fafec7ee70204403c8139d9956da054
Domaman202 <vip.domaman@mail.ru>
2021-10-11 23:17:01 +0700

    small fix

--- src/main/java/ru/DmN/core/item/VoltmeterItem.java
+++ src/main/java/ru/DmN/core/item/VoltmeterItem.java
@@ -7,6 +7,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import ru.DmN.core.api.energy.IESGetter;
 import ru.DmN.core.api.energy.IESObject;
 import ru.DmN.core.client.DCoreClient;
@@ -20,10 +22,15 @@ public class VoltmeterItem extends Item {
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        BlockEntity entity = context.getWorld().getBlockEntity(context.getBlockPos());
+        World world = context.getWorld();
+        if (world.isClient)
+            return ActionResult.SUCCESS;
+        BlockEntity entity = world.getBlockEntity(context.getBlockPos());
         if (entity instanceof IESGetter<?>) {
             IESObject<BlockEntity> storage = ((IESGetter<BlockEntity>) entity).getEnergyStorage(entity);
             PlayerEntity player = context.getPlayer();
+            BlockPos pos = entity.getPos();
+            player.sendMessage(new LiteralText("Pos -> { X = " + pos.getX() + "; Y = " + pos.getY() + "; Z = " + pos.getZ() + " }"), false);
             player.sendMessage(new LiteralText("Energy -> " + storage.getEnergy()), false);
             player.sendMessage(new LiteralText("MaxEnergy -> " + storage.getMaxEnergy()), false);
         }
