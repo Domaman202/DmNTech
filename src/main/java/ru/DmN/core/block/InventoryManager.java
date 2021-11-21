6e2fd3a260ffde5156248e2e7f1fc2169e070981
Domaman202 <vip.domaman@mail.ru>
2021-11-21 10:52:20 +0700

    pre beta 1.71

--- src/main/java/ru/DmN/core/block/InventoryManager.java
+++ src/main/java/ru/DmN/core/block/InventoryManager.java
@@ -8,17 +8,21 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import ru.DmN.core.block.entity.InventoryManagerBE;
 import ru.DmN.core.utils.Lazy;
+
+import java.util.ArrayList;
 
 import static ru.DmN.core.registry.GlobalRegistry.DEFAULT_ITEM_SETTINGS;
 
@@ -33,10 +37,65 @@ public class InventoryManager extends BlockWithEntity implements BlockEntityTick
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient)
+            return ActionResult.SUCCESS;
+        //
+        try {
+            ItemStack stack;
+            if ((stack = player.getMainHandStack()).getItem() instanceof WritableBookItem) {
+                var pages = (NbtList) stack.getOrCreateNbt().get("pages");
+                if (pages != null) {
+                    var code = new StringBuilder();
+                    for (var page : pages)
+                        code.append(page.asString());
+
+                    InventoryManagerBE entity = (InventoryManagerBE) world.getBlockEntity(pos);
+
+                    entity.tasks.clear();
+
+                    Direction dir0 = null;
+                    int slot0 = 0;
+                    Direction dir1 = null;
+                    int slot1 = 0;
+
+                    int i = 0;
+                    for (var str : code.toString().split("\n")) {
+                        switch (i) {
+                            case 0 -> dir0 = ofString(str);
+                            case 1 -> slot0 = Integer.parseInt(str);
+                            case 2 -> dir1 = ofString(str);
+                            case 3 -> slot1 = Integer.parseInt(str);
+                            case 4 -> {
+                                entity.addTask(slot0, slot1, dir0, dir1);
+                                i = -1;
+                            }
+                        }
+                        i++;
+                    }
+                }
+                return ActionResult.SUCCESS;
+            }
+        } catch (Exception e) {
+            player.sendMessage(new LiteralText("Parse Error!"), false);
+        }
         // Screen Open
         player.openHandledScreen((NamedScreenHandlerFactory) world.getBlockEntity(pos));
         //
         return ActionResult.SUCCESS;
+    }
+
+    /// TODO: MOVE TO UTILS
+
+    public static Direction ofString(String str) {
+        return switch (str.toLowerCase()) {
+            case "d" -> Direction.DOWN;
+            case "u" -> Direction.UP;
+            case "n" -> Direction.NORTH;
+            case "s" -> Direction.SOUTH;
+            case "w" -> Direction.WEST;
+            case "e" -> Direction.EAST;
+            default -> throw new IllegalStateException("Unexpected value: " + str);
+        };
     }
 
     ///
@@ -54,7 +113,7 @@ public class InventoryManager extends BlockWithEntity implements BlockEntityTick
         if (world.isClient)
             return;
 
-        for (var task : entity.tasks) // entity.addTask(0, 0, Direction.NORTH, Direction.SOUTH);
+        for (var task : entity.tasks)
             task.execute(world);
     }
 
